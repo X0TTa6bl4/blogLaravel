5e5e3c2585d1522b73c85603bfaaf83300939175
Bazlov Maxim <ccmmw@yandex.ru>
2020-10-11 15:58:18 +0000

    Реализована связь 1 ко многим

---
+++ app/Models/Step.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Step extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function task($relations)
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function complete($completed = true)
+    {
+        $this->update(['completed' => $completed]);
+    }
+
+    public function incomplete($completed = true)
+    {
+        $this->complete(false);
+    }
+}
